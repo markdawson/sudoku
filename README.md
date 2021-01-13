1ee93dafc6a8ac57458b7dd467b7ead96d0c6f7d
markdawson <mark@markdawson.io>
2021-01-12 22:45:29 -0800

    works without constraint satisfaction heauristics

--- README.md
+++ README.md
@@ -1,9 +1,69 @@
 # Simple Sudoku Solver
 
-This is a plain Python sudoku solver. 
+This is a plain Python sudoku solver that can solve any sudoku puzzle through a
+combination of constraint satisfaction and brute force search (using a simple heuristic
+to decide which nodes to search next).
 
-# Usage
+## Usage
+To see how the program works, simply type:
 
+`python sudoku_solver.py`
 
+It will print out an example puzzle and a solution to that puzzle. 
 
-# Implementation Details
+The program reads in a puzzle like this:
+```
+__2 | ___ | __5
+7_8 | 6_9 | _2_
+534 | 782 | ___
+----+-----+-----
+___ | 5__ | 4__
+19_ | 2_4 | _83
+__5 | __8 | ___
+----+-----+-----
+___ | 321 | 658
+_5_ | 9_6 | 7_2
+6__ | ___ | 9__
+```
+
+in the following format:
+
+`..2.....57.86.9.2.534782......5..4..19.2.4.83..5..8......321658.5.9.67.26.....9..`
+
+That is - Type all 81 characters of the puzzle from left to right, top to bottom with dots
+as empty squares. We can call this format a "puzzle string".
+
+It's recommend you put the puzzle string in a `.txt` file, but you can also feed in this string to the
+`solve()` function directly. The solve function is the only function you need to use.
+
+## Implementation Details
+This program represents the sudoku puzzle internally as a dictionary where the keys are one of 
+81 boxes (labeled A1, A1, ... I9) and the values are a string (like "1359") of all the possible 
+numbers that could be in that box. A box is considered solved if there is only one possible number 
+for that box. 
+
+Each box is part of multiple "peer groups". A "peer group" consists of 9 boxes that must contain the 
+numbers 1 through 9 exactly once. If two boxes are in the same "peer group" we can call them "peers", 
+thus "A1" and "A8" are peers because they both in the first row.
+
+This solver works by first trying three types of constraint satisfaction on the Sudoku puzzle, and then
+if the puzzle is still not solved, it finds the box with the fewest possibilities and then does a brute 
+force search on those possibilities. (This could also be described as tree traversal.)
+
+The three constraint satisfaction heuristics we use are:
+    
+`elimination` - If a box is solved, eliminate it's value from all it's peer's possibilities. For example,
+                if `C6` is `4`, then eliminate `4` as a possibility for all the boxes in the C row or 6th column
+                or the central box of the Sudoku puzzle.
+                
+`only_choice` - If is the only box in a peer group to contain a specific number then it must be that number.
+                For example if `E3` has possibilities `278`, but it is the only box in the E row to contain a `2`, 
+                then `E3` must contain `2`.
+                
+`naked_twins` - Our most complicated constraint satisfaction heuristic. If there are 3 boxes unsolved in a peer group 
+                and 2 of the 3 boxes in a group only have two possibilities, the possibility in the third box that is 
+                not in the other two must be the value of the third box. For example suppose grid looks like 
+                `{"A1": "157", "A2": "57", "A3": "57", ...}`. Then `A1` contain value `1` because `A2` and `A3` must
+                contain `5` and `7`.
+                
+Note that, we could, of course, solve  
--- sudoku_solver.py
+++ sudoku_solver.py
@@ -21,13 +21,19 @@ __5 | 2__ | ___
 ___ | ___ | __3
 """
 import re
+from collections import Counter
+from uuid import uuid4
 
 assignments = []
+
+NUM_SEARCHES = 0
 
 
 # Define some helpful global variables
 def cross(A, B):
-    """Cross product of elements in A and elements in B."""
+    """
+    Cross product of elements in A and elements in B.
+    """
     return [a + b for a in A for b in B]
 
 
@@ -41,15 +47,14 @@ column_units = [cross(rows, col) for col in cols]
 square_units = [cross(row, col)
                 for row in ['ABC', 'DEF', 'GHI']
                 for col in ['123', '456', '789']]
-unit_list = row_units + column_units + square_units
-units = dict((s, [u for u in unit_list if s in u]) for s in boxes)
-peers = dict((s, set(sum(units[s], [])) - {s}) for s in boxes)
+peer_group_list = row_units + column_units + square_units
+peer_group = dict((s, [u for u in peer_group_list if s in u]) for s in boxes)
+peers = dict((s, set(sum(peer_group[s], [])) - {s}) for s in boxes)
 
 
 def assign_value(values, box, value):
     """
-    Please use this function to update your values dictionary!
-    Assigns a value to a given box. If it updates the board record it.
+    This
     """
     values[box] = value
     if len(value) == 1:
@@ -57,11 +62,11 @@ def assign_value(values, box, value):
     return values
 
 
-def naked_twins(values):
+def naked_twins(grid_dict):
     """Eliminate values using the naked twins strategy.
 
     Args:
-        values(dict): a dictionary of the form {'box_name': '123456789', ...}
+        grid_dict(dict): a dictionary of the form {'box_name': '123456789', ...}
 
     Returns:
         the values dictionary with the naked twins eliminated from peers.
@@ -69,24 +74,23 @@ def naked_twins(values):
 
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
-    from collections import Counter
-    for unit in unit_list:
+    for unit in peer_group_list:
         # Collect all the twins in a unit in case there are multiple twins in one unit
         twins = [digits for digits, count in
-                 Counter(values[box] for box in unit if len(values[box]) == 2).items()
+                 Counter(grid_dict[box] for box in unit if len(grid_dict[box]) == 2).items()
                  if count > 1]
 
         for twin in twins:
             for box in unit:
-                if set(values[box]) == set(twin):
+                if set(grid_dict[box]) == set(twin):
                     # Skip if this box is exactly equal to the twin
                     # Take set in case order got mixed up
                     continue
                 for digit in twin:
-                    if digit in values[box]:
-                        new_value = values[box].replace(digit, '')
-                        assign_value(values, box, new_value)
-    return values
+                    if digit in grid_dict[box]:
+                        new_value = grid_dict[box].replace(digit, '')
+                        assign_value(grid_dict, box, new_value)
+    return grid_dict
 
 
 def sudoku_input_string_to_grid_dict(grid):
@@ -108,6 +112,9 @@ def display(grid_dict: dict, debugging_display: bool = False):
     :param grid_dict: A dictionary of the squares and potential values for each square.
     :param debugging_display: Whether we are displaying for debugging purposes or output purposes.
     """
+    if not grid_dict:
+        return
+
     width = max(len(grid_dict[k]) for k in boxes) + 1 if debugging_display else 1
     for i, row in enumerate(rows):
         if i % 3 == 0:
@@ -146,7 +153,7 @@ def only_choice(values):
     Assign a box to a value if it's the only box
     in a unit that could contain that value
     """
-    for unit in unit_list:
+    for unit in peer_group_list:
         occurs_only_once = set()
         occurs_more_than_once = set()
         for box in unit:
@@ -166,65 +173,6 @@ def only_choice(values):
     return values
 
 
-def verify_no_empty_boxes(grid_dict, message):
-    empty_boxes = [box for box in grid_dict.keys() if len(grid_dict[box]) == 0]
-    if empty_boxes:
-        print(message)
-        raise Exception(f"We've eliminated all the possibilities for a box: {empty_boxes}")
-
-
-def reduce_puzzle(grid_dict):
-    stalled = False
-    while not stalled:
-        if isinstance(grid_dict, str):
-            print('Values is {}'.format(grid_dict))
-        number_solved_before = len([box for box in grid_dict.keys() if len(grid_dict[box]) == 1])
-
-        verify_no_empty_boxes(grid_dict, "Before eliminate")
-        grid_dict = eliminate(grid_dict)
-        verify_no_empty_boxes(grid_dict, "After eliminate")
-
-        grid_dict = only_choice(grid_dict)
-        verify_no_empty_boxes(grid_dict, "After only choice")
-
-        number_solved_after = len([box for box in grid_dict.keys() if len(grid_dict[box]) == 1])
-        stalled = number_solved_before == number_solved_after
-
-        # Sanity check
-        verify_no_empty_boxes(grid_dict, "At end of while loop")
-
-    print("")
-    display(grid_dict)
-    return grid_dict
-
-
-def search(grid_dict):
-    grid_dict = reduce_puzzle(grid_dict)
-    if grid_dict is False:
-        raise Exception("Something broke in reduce puzzle")
-        return False
-
-    if is_solved(grid_dict):
-        print("The following grid should be solved:")
-        print(grid_dict)
-        print("============>" * 6)
-        return grid_dict
-
-    min_val, min_box = min((len(grid_dict[box]), box) for box in boxes if len(grid_dict[box]) > 1)
-    for possibility in grid_dict[min_box]:
-        print()
-        print(f"Exploring possibility: {possibility} in box {min_box}...")
-        new_search_grid_dict = grid_dict.copy()
-        new_search_grid_dict[min_box] = possibility
-        attempt = search(new_search_grid_dict)
-        if is_solved(attempt):
-            return attempt
-
-
-def is_solved(grid_dict: dict) -> bool:
-    return all(len(val) == 1 for val in grid_dict.values())
-
-
 def grid_to_output_format(grid_dict: dict) -> str:
     if not is_solved(grid_dict):
         raise Exception(f"This grid is not solved: {grid_dict}")
@@ -235,7 +183,62 @@ def grid_to_output_format(grid_dict: dict) -> str:
         return result
 
 
-def solve(sudoku_input: str, print_results: bool = True):
+def fill_in_with_constraint_satisfaction(grid_dict):
+    stalled = False
+    while not stalled:
+        number_solved_before = len([box for box in grid_dict.keys() if len(grid_dict[box]) == 1])
+
+        grid_dict = eliminate(grid_dict)
+        grid_dict = only_choice(grid_dict)
+        grid_dict = naked_twins(grid_dict)
+
+        number_solved_after = len([box for box in grid_dict.keys() if len(grid_dict[box]) == 1])
+        stalled = number_solved_before == number_solved_after
+
+    return grid_dict
+
+
+def is_valid(grid_dict: dict) -> bool:
+    for group in peer_group_list:
+        group_values = set()
+        for box in group:
+            box_value = grid_dict[box]
+            if len(box_value) == 1:
+                if box_value in group_values:
+                    return False
+                else:
+                    group_values.add(grid_dict[box])
+
+    return True
+
+
+def is_solved(grid_dict: dict) -> bool:
+    return grid_dict and is_valid(grid_dict) and all(len(val) == 1 for val in grid_dict.values())
+
+
+def get_min_box(grid_dict):
+    min_val, min_box = min((len(grid_dict[box]), box) for box in boxes if len(grid_dict[box]) > 1)
+    return min_box
+
+
+def search(grid_dict, use_constraint_satisfaction_heuristics=True):
+    if use_constraint_satisfaction_heuristics:
+        grid_dict = fill_in_with_constraint_satisfaction(grid_dict)
+
+    if is_solved(grid_dict):
+        return grid_dict
+
+    min_box = get_min_box(grid_dict)
+    for possibility in grid_dict[min_box]:
+        new_search_grid_dict = grid_dict.copy()
+        new_search_grid_dict[min_box] = possibility
+        if is_valid(new_search_grid_dict):
+            attempt = search(new_search_grid_dict, use_constraint_satisfaction_heuristics)
+            if is_solved(attempt):
+                return attempt
+
+
+def solve(sudoku_input: str, use_constraint_satisfaction_heuristics: bool = True, print_results: bool = True,):
     """
     Takes an input string and returns a grid with the corresponding values filled in.
 
@@ -250,17 +253,19 @@ def solve(sudoku_input: str, print_results: bool = True):
     input_grid = sudoku_input_string_to_grid_dict(sudoku_input)
 
     if print_results:
+        print("..........................Solving.........................")
         display(input_grid)
 
-    solution_grid = search(input_grid)
+    solution_grid = search(input_grid, use_constraint_satisfaction_heuristics)
 
     if print_results:
+        print("..........................Solved..........................")
         display(solution_grid)
 
     return grid_to_output_format(solution_grid)
 
 
 if __name__ == "__main__":
-    with open("sudoku_puzzle1.txt") as f:
+    with open("sudoku_puzzle3.txt") as f:
         sample_input = f.read().strip()
         output = solve(sample_input)
